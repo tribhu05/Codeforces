ab3e6dd05948f84e35ee31187434b88f79289741
Tribhuwan Singh <tribhuwansingh5050@gmail.com>
2026-08-03 22:38:34 +0530

    Add files via upload

---
+++ 617A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    int count=0;
+    cin>>x;
+    count=x/5;
+    if(x!=0){
+        
+        count++;
+    }
+    cout<<count;
+    return 0;
+}
